ec49dba759e68c739841f15797027f77a21d23f2
roodhouse <rughjm@gmail.com>
2023-06-15 12:55:09 -0500

    setup tailwind, colors and fonts

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+       <h1 className="text-3xl font-bold underline font-['Fraunces'] text-deepAqua">
+      Hello <span className='font-["Montserrat"] text-gunmetal'>world!</span>
+    </h1>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      white: '#ffffff',
+      cream: '#F2EAE2',
+      aurometal: '#6C7289',
+      deepAqua: '#3D8168',
+      deepAquaHover: '#1A4032',
+      gunmetal: '#1C232B'
+    },
+  },
+  plugins: [],
+}
